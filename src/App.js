26d336c388b4f988dd55489ba26ad4c5e4174127
ttek <dkakol01@gmail.com>
2019-04-30 21:33:09 +0200

    set basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import Piece from "./components/Piece";
+
+function Game() {
+  const [values, setValues] = useState(Array(8).fill(Array(8).fill("none")));
+
+  const Board = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 600px;
+    height: 600px;
+    align-items: center;
+    justify-content: center;
+    background-color: burlywood;
+  `;
+
+  return <Board>{values.map(val => val.map(v => <div>{v}</div>))}</Board>;
+}
 
 function App() {
+  const GlobalStyle = createGlobalStyle`
+  body{
+   margin:0;
+   padding:0;
+   background:linear-gradient(45deg , blue,violet);
+   height:100vh;
+   width:100vw;
+  }
+  .App{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height:100vh;
+    width:100%
+  }
+  `;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <Game />
     </div>
   );
 }
---
+++ src/components/Piece.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Piece = () => {
+  return <div>working</div>;
+};
+
+export default Piece;
